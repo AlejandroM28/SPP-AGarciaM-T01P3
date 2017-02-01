97ad25de0b69b96454e057cdb4d320787cd786e3
alejandromonroy <alejandromonroy@10.34.147.240>
2017-01-31 20:32:46 -0600

    Programa 3 de 3.

--- src/spp/agarciam_t01/p1/sppagarciamt01p1.java
+++ src/spp/agarciam_t01/p1/sppagarciamt01p1.java
@@ -16,22 +16,24 @@ public class sppagarciamt01p1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int A1,A2,B1,B2;
+        int X1,X2,Y1,Y2;
         double D;
         // A1, A2, B1, B2 = COORDENADAS, D= DISTANCIA
-        Scanner T= new Scanner (System.in);
+        Scanner kb= new Scanner (System.in);
         
-        System.out.println("Escribe el valor de A1");
-        A1 = (int) T.nextDouble();
-        System.out.println("Escribe el valor de A2");
-        A2 = (int) T.nextDouble();
-        System.out.println("Escribe el valor de B1");
-        B1 = (int) T.nextDouble();
-        System.out.println("Escribe el valor de B2");
-        B2 = (int) T.nextDouble();
+        System.out.println("Escribe el valor de X1");
+        X1 = (int) kb.nextDouble();
+        System.out.println("Escribe el valor de X2");
+        X2 = (int) kb.nextDouble();
+        System.out.println("Escribe el valor de Y1");
+        Y1 = (int) kb.nextDouble();
+        System.out.println("Escribe el valor de Y2");
+        Y2 = (int) kb.nextDouble();
         
-        D = Math.sqrt (Math.pow((A1-A2),2) + Math.pow((B1-B2),2));
+        D = Math.sqrt (Math.pow((X1-X2),2) + Math.pow((Y1-Y2),2));
         System.out.println("La distancia entre los puntos es: " + D);
+       
+        
     }
     
 }
